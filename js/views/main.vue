ed448b9ca4ef15a02db3fa32df2d145a0cd6f509
JiauZhang <1743960454@qq.com>
2025-02-16 11:53:26 +0800

    seperate litegraph code

--- js/views/main.vue
+++ js/views/main.vue
@@ -1,3 +1,10 @@
+
+<script setup>
+import {LiteGraph} from '@/lib/litegraph.js'
+import LGraph from '@/lib/LGraph';
+console.log(LGraph, LiteGraph)
+</script>
+
 <template>
     <p>hello litegraph</p>
 </template>
