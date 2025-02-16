6a5573ff550d76ba0759ec590689bdbd8d28df7c
JiauZhang <1743960454@qq.com>
2025-02-16 15:48:05 +0800

    split code

---
+++ js/litegraph/index.js
@@ -0,0 +1,13 @@
+import LiteGraph from './LiteGraph'
+import LGraphCanvas from "./LGraphCanvas";
+import LGraph from "./LGraph";
+import LLink from "./LLink";
+import LGraphNode from "./LGraphNode";
+import LGraphGroup from "./LgraphGroup";
+import DragAndScale from "./DragAndScale";
+import ContextMenu from "./ContextMenu";
+import CurveEditor from "./CurveEditor";
+
+LiteGraph.LGraphCanvas = LGraphCanvas;
+
+export { LGraph, LLink, LGraphNode, LGraphGroup, DragAndScale, ContextMenu, CurveEditor, LiteGraph, LGraphCanvas };
